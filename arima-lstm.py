3aac175a7879ca6451a3cfeb3d1e2b8e35868368
Maradonna <marco.jendryczko@gmx.net>
2020-01-14 17:27:15 +0100

    added lstm and hybrid checking for best proportions

--- arima-lstm.py
+++ arima-lstm.py
@@ -12,13 +12,19 @@ import pickle
 from sklearn.linear_model import Lasso, HuberRegressor
 from statsmodels.tsa.arima_model import ARIMA
 from functools import reduce
-
+from math import sqrt
 from matplotlib import pyplot
+
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras import backend as K
 def main():
     #TODO: create a predictor from ws data to kickerpoints to fill out gaps
     #preprocessing
+    #TODO: rolling forecast through all years beginning from 0-2 train and predict 3
     r = Reader()
-    seasons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    seasons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
     season_X = []
     season_Y = []
     clubs = ws_load_database("all_clubs")
@@ -26,10 +32,10 @@ def main():
     for season in seasons:
         season_x, _ =  r.read("lstm_data/"+str(season).zfill(2)+".csv")
         season_x = pd.DataFrame(season_x)
-        #ws_refresh_database("all_names", season_x['name'])
-        #ws_refresh_database("all_clubs", season_x['club'])
-        #clubs = ws_load_database("all_clubs")
-        #names = ws_load_database("all_names")
+        ws_refresh_database("all_names", season_x['name'])
+        ws_refresh_database("all_clubs", season_x['club'])
+        clubs = ws_load_database("all_clubs")
+        names = ws_load_database("all_names")
         season_x['name'] = season_x.apply(lambda x: names[x['name']], axis=1)
         season_x['club'] = season_x.apply(lambda x: clubs[x['club']], axis=1)
         season_X.append(season_x)
@@ -39,52 +45,61 @@ def main():
     remove_cols = ['position_x', 'position_y', 'age_x', 'age_y', 'club_x', 'club_y']
     df_names = reduce(lambda left,right: pd.merge(left, right, on='name'), season_X[-4:])
     df_names = df_names.drop(remove_cols, axis=1)
-    print(df_names.head())
+    #print(df_names.head())
     predictions = list()
+    print("ARIMA running...")
     for (i, row) in df_names.iterrows():
         series = row.iloc[1:-34]
         # training sets should just contain player that are in prediction set and all season of prediction set
         series = np.asarray(series, dtype=int)
-        model = ARIMA(series, order=(5,1,0))
-        model_fit = model.fit(disp=0)
+        model = ARIMA(series, order=(7,1,0))
+        model_fit = model.fit(disp=0, trend='c')
         output = model_fit.forecast(steps=34)
         yhat = output[0]
         predictions.append(yhat)
         obs = row.iloc[-34:]
         #print('predicted={}, expected={}'.format(yhat, obs))
     test = df_names.iloc[:,-34:]
-    print(test)
-    error = mean_squared_error(test, predictions)
-    print('Test MSE: %.3f' % error)
-    pyplot.plot(test.iloc[1,:])
-    pyplot.plot(predictions[1], color='red')
-    pyplot.show()
-def run_model(name, model, data, seasons, posis, predict=False):
-    print("train and predict "+name)
-    season_index = len(seasons)
-    if predict:
-        season_index = -1
-    mean_error = []
-    pred = None
-    dat = None
-    real = None
-    for season in seasons[1:]:
-        train = data[data['season'] < season]
-        val = data[data['season'] == season]
-        error = mean_squared_error(yts, pred)
-        print('Season %d - Error %.5f' % (season, error))
-        mean_error.append(error)
-    print('Mean Error = %.5f' % np.mean(mean_error))
-    #return np.mean(mean_error)
-    res = []
-    clubs = ws_load_database("clubs")
-    names = ws_load_database("names")
-    dat = pd.DataFrame(dat)
-    for player, test, prediction in zip(dat.iterrows(), real, pred):
-        player = player[1]
-        res.append([names.inverse[int(player['name'])][0], int(player['age']), clubs.inverse[int(player['club'])][0], posis.inverse[int(player['position'])][0], int(test), prediction])
-    write(name+"-lstm-pred", res)
+    #print(test)
+    error = sqrt(mean_squared_error(test, predictions))
+    print('Test RMSE: %.3f' % error)
+    #pyplot.plot(test.iloc[1,:])
+    #pyplot.plot(predictions[1], color='red')
+    #pyplot.show()
+    #+++++++++++++++++
+    #+ LSTM TRAINING +
+    #+++++++++++++++++
+    print("Running LSTM...")
+    output_dim = 34
+    train = df_names.iloc[:, 1:-34]
+    test = df_names.iloc[:, -34:]
+    n_steps = 3*34
+    n_features = 1
+    train = train.to_numpy()
+    train = train.reshape((train.shape[0], train.shape[1], n_features))
 
+    test = test.to_numpy()
+    #test = test.reshape((test.shape[0], test.shape[1], n_features))
+
+    # define model
+    # tf.keras.metrics.RootMeanSquaredError()
+    model = Sequential()
+    model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Dense(34))
+    model.compile(optimizer='adam', loss=root_mean_squared_error)
+    # fit model
+    model.fit(train, test, epochs=200, verbose=0)
+    # demonstrate prediction
+    yhat = model.predict(train, verbose=0)
+    predictions = np.array(predictions)
+    for p in np.linspace(0,1,11):
+        #print(type(p), type(predictions), type(yhat), predictions.dtype, yhat.dtype)
+        y_hybrid = (predictions * p) + (yhat * (1-p))
+        err = root_mean_squared_error(np.array(test, dtype='float64'), y_hybrid)
+        print(p, "RMSE:", err.numpy())
+
+def root_mean_squared_error(y_true, y_pred):
+    return K.sqrt(K.mean(K.square(y_pred - y_true)))
 def write(filename, res):
     with open(filename+'.csv', 'w', newline='\n') as csvfile:
         writer = csv.writer(csvfile)
